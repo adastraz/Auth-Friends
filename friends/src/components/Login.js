dd61edd96f219cc8ef3facdfcead6f60ee9cbbaf
adastraz <tylergosox@gmail.com>
2020-01-28 16:40:28 -0500

    getting there

---
+++ friends/src/components/Login.js
@@ -0,0 +1,46 @@
+import React, {useState} from 'react'
+import axios from 'axios'
+
+const Login = () => {
+    const [user, setUser] = useState({
+        username: '',
+        password: ''
+    })
+    const handleChanges = e => {
+       setUser({
+           ...user, [e.target.name]: e.target.value
+       })
+    }
+    const login = e => {
+        e.preventDefault()
+        axios.post('http://localhost:5000/api/login', user)
+            .then(res => {
+                localStorage.setItem('token', res.data.payload)
+                props.history.push('/protected')
+            })
+            .catch(err => console.log(err))
+    }
+    return(
+        <div>
+            <form onSubmit={(user => login(user))}>
+                <input
+                    type='text'
+                    name='username'
+                    value={user.username}
+                    placeholder='username'
+                    onChange={(e => handleChanges(e))}
+                />
+                <input
+                type='password'
+                name='password'
+                value={user.password}
+                placeholder='password'
+                onChange={(e => handleChanges(e))}
+            />
+            <button>Login</button>
+            </form>
+        </div>
+    )
+}
+
+export default Login
